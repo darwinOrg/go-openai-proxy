c32ab6f852bbe66a8e3f74b9518b4cf8b3d86100
chenbiao <149027310@qq.com>
2023-10-20 16:57:07 +0800

    refactor

--- proxy.go
+++ proxy.go
@@ -11,6 +11,8 @@ import (
 	"time"
 )
 
+var DefaultClient *openai.Client
+
 // NewProxyClient creates new OpenAI API proxy client.
 func NewProxyClient(proxyBaseUrl string) *openai.Client {
 	return NewProxyClientWithToken(proxyBaseUrl, "none")
@@ -21,6 +23,10 @@ func NewProxyClientWithToken(proxyBaseUrl string, authToken string) *openai.Clie
 	config := openai.DefaultConfig(authToken)
 	config.BaseURL = proxyBaseUrl
 	return openai.NewClientWithConfig(config)
+}
+
+func DefaultSimpleChatCompletion(ctx *dgctx.DgContext, request openai.ChatCompletionRequest) (string, error) {
+	return SimpleChatCompletion(DefaultClient, ctx, request)
 }
 
 func SimpleChatCompletion(client *openai.Client, ctx *dgctx.DgContext, request openai.ChatCompletionRequest) (string, error) {
@@ -37,12 +43,20 @@ func SimpleChatCompletion(client *openai.Client, ctx *dgctx.DgContext, request o
 	return response.Choices[0].Message.Content, nil
 }
 
+func DefaultCreateChatCompletion(ctx *dgctx.DgContext, request openai.ChatCompletionRequest) (openai.ChatCompletionResponse, error) {
+	return CreateChatCompletion(DefaultClient, ctx, request)
+}
+
 func CreateChatCompletion(client *openai.Client, ctx *dgctx.DgContext, request openai.ChatCompletionRequest) (openai.ChatCompletionResponse, error) {
 	start := time.Now().UnixMilli()
 	response, err := client.CreateChatCompletion(context.Background(), request)
 	dglogger.Infof(ctx, "create chat completion, request: %+v, response: %+v, error: %v, cost: %d ms",
 		request, response, err, time.Now().UnixMilli()-start)
 	return response, err
+}
+
+func DefaultCreateCompletion(ctx *dgctx.DgContext, request openai.CompletionRequest) (openai.CompletionResponse, error) {
+	return CreateCompletion(DefaultClient, ctx, request)
 }
 
 func CreateCompletion(client *openai.Client, ctx *dgctx.DgContext, request openai.CompletionRequest) (openai.CompletionResponse, error) {
